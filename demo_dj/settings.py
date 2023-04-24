9d4c10ce51fb33adb821cab1e94ef9672bdcad95
colorblackful_zhouzhou <124777439+colorfbalck@users.noreply.github.com>
2023-04-24 15:05:23 +0800

    Update settings.py

--- demo_dj/settings.py
+++ demo_dj/settings.py
@@ -121,21 +121,6 @@ WSGI_APPLICATION = 'demo_dj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dj_test',
-        'USER': 'root',
-        'PASSWORD': 'Ｍak&@ia0Dbe_',
-        "HOST": 'mariadb.idmakers.cn',
-        'PORT': 9381
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
